909e560574c7e8237a2bdc82ed18964aee6b6ab8
Wez Furlong <wez@netevil.org>
2010-05-22 18:33:23 -0400

    add google tracking

--- inc/layout.php
+++ inc/layout.php
@@ -154,6 +154,21 @@ function wfo_head($title, $area = 'home')
     </script>
   </head>
   <body>
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-1250469-3']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 
+              'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+
     <div id="navtitle">
       <div id="tpad">
         <div id="maintitle"><a href="$WEBROOT">$AREA_TITLE</a></div>
