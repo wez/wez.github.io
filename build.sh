7fb2bed52b04b5a5137a44756e351abf7d52fed3
Wez Furlong <wez@wezfurlong.org>
2025-02-07 13:11:58 -0700

    tweak how the feeds file is generated

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 mkdir -p gh_pages
-docker run --rm -e CARDS=true -v ${PWD}:/docs squidfunk/mkdocs-material build
-# Add symlink to preserve old feed url operation
-cd gh_pages && ln -s feed_rss_created.xml feed.xml
+docker run --pull=always --rm -e CARDS=true -v ${PWD}:/docs squidfunk/mkdocs-material build
