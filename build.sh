20d8b9bdca8cd487c47c7242cabfb876a005d09c
Wez Furlong <wez@wezfurlong.org>
2025-08-05 07:34:40 +0100

    more pain with mkdocs material and feed.xml

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 mkdir -p gh_pages
 docker run --pull=always --rm -e CARDS=true -v ${PWD}:/docs squidfunk/mkdocs-material build
+# Add symlink to preserve old feed url operation
+cd gh_pages && ln -s feed_rss_created.xml feed.xml
